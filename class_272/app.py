d9bbd53e04ae9541105f1a91a595259385375b6e
aakritiverma408 <73687369+aakritiverma408@users.noreply.github.com>
2024-01-05 18:55:40 +0530

    Add files via upload

---
+++ class_272/app.py
@@ -0,0 +1,54 @@
+import os
+
+from flask import Flask, request, jsonify, render_template, redirect, url_for,send_file
+from faker import Faker
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import SyncGrant
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+fake = Faker()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Add your Twilio credentials
+@app.route('/token')
+def generate_token():
+    TWILIO_ACCOUNT_SID = 'AC63cb2e3129f945aa71f139f668f32708'
+    TWILIO_SYNC_SERVICE_SID = 'IS44c80685451c62d7d996a204befd385a'
+    TWILIO_API_KEY = 'SKc18b58a2e82794204b06a2856b52ab8d'
+    TWILIO_API_SECRET = 'teDAmLXngucMZPlOaVTSXgbkz8txn9ML'
+
+    username = request.args.get('username', fake.user_name())
+
+    # create access token with credentials
+    token = AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET, identity=username)
+    # create a Sync grant and add to token
+    sync_grant_access = SyncGrant(TWILIO_SYNC_SERVICE_SID)
+    token.add_grant(sync_grant_access)
+    return jsonify(identity=username, token=token.to_jwt().decode())
+
+# Write the code here
+@app.route('/', methods=['POST'])
+def download_text():
+    text_from_notepad = request.form['text']
+    with open('workfile.txt','w') as f:
+        f.write(text_from_notepad)
+
+    path_to_store_txt = "workfile.txt"
+
+    return send_file(path_to_store_txt, as_attachment=True)
+    
+    
+        
+
+    
+
+    
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port='5001', debug=True)
